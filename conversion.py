da51240c18219a07038cf772b975c108b28c1af0
ARUN KUMAR K <93638254+arun3583@users.noreply.github.com>
2021-12-01 09:17:56 +0530

    Temperature convertion

---
+++ conversion.py
@@ -0,0 +1,8 @@
+a=float(input("enter temperature in degree celsius"))
+c=(a * 9/5) + 32
+print("temperature in farehnheit",c)
+b=float(input("enter temperature in farehnheat"))
+e=(b - 32) * 5/9
+print("temperature in celsius",e)
+
+
