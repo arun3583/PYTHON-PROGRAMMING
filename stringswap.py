0eabb09ab9754fbe05b598a8c2419709aef1556e
ARUN KUMAR K <93638254+arun3583@users.noreply.github.com>
2021-12-20 11:33:59 +0530

    String 1st character swap

--- stringswap.py
+++ stringswap.py
@@ -1,8 +1,8 @@
-a=input("Enter a string:")
-b=input("Enter a string:")
-d=a[0:2]
-e=b[0:2]
-f=d+b[2:]
-g=e+a[2:]
+s=input("Enter a string seperated by space:").split(" ")
+a=s[0]
+b=s[1]
+d=a[0]
+e=b[0]
+f=d+b[1:]
+g=e+a[1:]
 print(g," ",f)
-
